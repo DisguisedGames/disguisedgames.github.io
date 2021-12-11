dfd5103079b5e4de7d0226792f9fa77867fb9f96
JohanDev <johanclipss@gmail.com>
2021-12-11 21:53:33 +0100

    static image optimizer

--- next.config.js
+++ next.config.js
@@ -1,3 +1,12 @@
-module.exports = {
-  reactStrictMode: true,
-}
+const withPlugins = require('next-compose-plugins');
+const optimizedImages = require('next-optimized-images');
+
+module.exports = withPlugins([
+  {
+    reactStrictMode: true,
+  },
+  [optimizedImages, {
+    /* config for next-optimized-images */
+  }],
+  
+]);
