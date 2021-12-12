63afb84ef5f8260821a9575608ab68d916355386
JohanDev <johanclipss@gmail.com>
2021-12-12 09:26:48 +0100

    maybe

--- components/GamesList.js
+++ components/GamesList.js
@@ -2,6 +2,17 @@ import styled from "styled-components";
 import Link from 'next/link';
 import Image from "next/image";
 
+import Vex3 from "./data/gameIcons/vex3.png";
+import Vex4 from "./data/gameIcons/vex4.png";
+import Vex5 from "./data/gameIcons/vex5.png";
+import Vex6 from "./data/gameIcons/vex6.png";
+import BasketballLegends2020 from "./data/gameIcons/basketball-legends-2020.png";
+import IdleStartupTycoon from "./data/gameIcons/idle-startup-tycoon.png";
+import Paperio2 from "./data/gameIcons/paperio-2.jpeg";
+import Aquaparkio from "./data/gameIcons/aquaparkio.jpeg";
+import TwoBall3D from "./data/gameIcons/two-ball-3d.jpeg";
+import Snake from "./data/gameIcons/snake.png";
+
 const sanityIoImageLoader = ({ src, width, quality }) => {
     return `https://cdn.sanity.io/${src}?w=${width}&q=${quality || 75}`
   }
@@ -79,70 +90,70 @@ const GameList = () => {
                         <Text>Vex3</Text>
                         <Describe>Vex3 Platfomer game.</Describe>
                         <Link href='/play/vex3' passHref>
-                            <StyledLink> <Image src={"/data/gameIcons/vex3.png"} alt="Game Image" height="232px" width="238px" /> </StyledLink>
+                            <StyledLink> <Image src={Vex3} alt="Game Image" height={232} width={238} loader={sanityIoImageLoader} /> </StyledLink>
                         </Link>
                     </Game>
                     <Game>
                         <Text>Vex4</Text>
                         <Describe>Vex4 Platfomer game.</Describe>
                         <Link href='/play/vex4' passHref>
-                            <StyledLink> <Image src={"/data/gameIcons/vex4.png"} alt="Game Image" height="232px" width="238px" /> </StyledLink>
+                            <StyledLink> <Image src={Vex4} alt="Game Image" height={232} width={238} loader={sanityIoImageLoader} /> </StyledLink>
                         </Link>
                     </Game>
                     <Game>
                         <Text>Vex5</Text>
                         <Describe>Vex5 Platfomer game.</Describe>
                         <Link href='/play/vex5' passHref>
-                            <StyledLink> <Image src={"/data/gameIcons/vex5.png"} alt="Game Image" height="232px" width="238px" /> </StyledLink>
+                            <StyledLink> <Image src={Vex5} alt="Game Image" height={232} width={238} loader={sanityIoImageLoader} /> </StyledLink>
                         </Link>
                     </Game>
                     <Game>
                         <Text>Vex6</Text>
                         <Describe>Vex6 Platfomer game.</Describe>
                         <Link href='/play/vex6' passHref>
-                            <StyledLink> <Image src={"/data/gameIcons/vex6.png"} alt="Game Image" height="232px" width="238px" /> </StyledLink>
+                            <StyledLink> <Image src={Vex6} alt="Game Image" height={232} width={238} loader={sanityIoImageLoader} /> </StyledLink>
                         </Link>
                     </Game>
                     <Game>
                         <Text>Snake</Text>
                         <Describe>The google snake game.</Describe>
                         <Link href='/play/snake' passHref>
-                            <StyledLink> <Image src={"/data/gameIcons/snake.png"} alt="Game Image" height="232px" width="238px" /> </StyledLink>
+                            <StyledLink> <Image src={Snake} alt="Game Image" height={232} width={238} loader={sanityIoImageLoader} /> </StyledLink>
                         </Link>
                     </Game>
                     <Game>
                         <Text>BL 2020</Text>
                         <Describe>Basketball Legends 2020.</Describe>
                         <Link href='/play/basketball-legends-2020' passHref>
-                            <StyledLink> <Image src={"/data/gameIcons/basketball-legends-2020.png"} alt="Game Image" height="232px" width="238px" /> </StyledLink>
+                            <StyledLink> <Image src={BasketballLegends2020} alt="Game Image" height={232} width={238} loader={sanityIoImageLoader} /> </StyledLink>
                         </Link>
                     </Game>
                     <Game>
                         <Text>IS Tycoon</Text>
                         <Describe>Idle Startup Tycoon.</Describe>
                         <Link href='/play/idle-startup-tycoon' passHref>
-                            <StyledLink> <Image src={"/data/gameIcons/idle-startup-tycoon.png"} alt="Game Image" height="232px" width="238px" /> </StyledLink>
+                            <StyledLink> <Image src={IdleStartupTycoon} alt="Game Image" height={232} width={238} loader={sanityIoImageLoader} /> </StyledLink>
                         </Link>
                     </Game>
                     <Game>
                         <Text>Paper.io 2</Text>
                         <Describe>Paper.io 2 IO game.</Describe>
                         <Link href='/play/paperio-2' passHref>
-                            <StyledLink> <Image src={"/data/gameIcons/paperio-2.jpeg"} alt="Game Image" height="232px" width="238px" /> </StyledLink>
+                            <StyledLink> <Image src={Paperio2} alt="Game Image" height={232} width={238} loader={sanityIoImageLoader} /> </StyledLink>
                         </Link>
                     </Game>
                     <Game>
                         <Text>Aquapark.io</Text>
                         <Describe>Aquapark.io IO game.</Describe>
                         <Link href='/play/aquaparkio' passHref>
-                            <StyledLink> <Image src={"/data/gameIcons/aquaparkio.jpeg"} alt="Game Image" height="232px" width="238px" /> </StyledLink>
+                            <StyledLink> <Image src={Aquaparkio} alt="Game Image" height={232} width={238} loader={sanityIoImageLoader} /> </StyledLink>
                         </Link>
                     </Game>
                     <Game>
                         <Text>Two Ball 3D</Text>
                         <Describe>Two Ball 3D game.</Describe>
                         <Link href='/play/two-ball-3d' passHref>
-                            <StyledLink> <Image src={"/data/gameIcons/two-ball-3d.jpeg"} alt="Game Image" height="232px" width="238px" /> </StyledLink>
+                            <StyledLink> <Image src={TwoBall3D} alt="Game Image" height={232} width={238} loader={sanityIoImageLoader} /> </StyledLink>
                         </Link>
                     </Game>
                 </Grid>
--- components/Navbar.js
+++ components/Navbar.js
@@ -2,6 +2,10 @@ import styled from 'styled-components';
 import Link from 'next/link';
 import Image from 'next/image';
 
+const sanityIoImageLoader = ({ src, width, quality }) => {
+    return `https://cdn.sanity.io/${src}?w=${width}&q=${quality || 75}`
+  }
+
 const Nav = styled.nav`
     height: 80px;
     background-color: #0e0e0e;
@@ -33,17 +37,17 @@ const Navbar = () => {
             <div>
                 <Link href='/games' passHref>
                     <StyledLink>
-                        <Image src={'/data/navIcons/games.svg'} alt="Games" width="25rem" height="25rem" />
+                        <Image src={'/data/navIcons/games.svg'} alt="Games" width={25} height={25} loader={sanityIoImageLoader} />
                     </StyledLink>
                 </Link>
                 <Link href='/' passHref>
                     <StyledLink>
-                        <Image src={'/data/navIcons/home.svg'} alt="Home" width="25rem" height="25rem" />
+                        <Image src={'/data/navIcons/home.svg'} alt="Home" width={25} height= {25} loader={sanityIoImageLoader} />
                     </StyledLink>
                 </Link>
                 <Link href='/settings' passHref>
                     <StyledLink>
-                        <Image src={'/data/navIcons/settings.svg'} alt="Settings" width="25rem" height="25rem" />
+                        <Image src={'/data/navIcons/settings.svg'} alt="Settings" width={25} height={25} loader={sanityIoImageLoader} />
                     </StyledLink>
                 </Link>
             </div>
