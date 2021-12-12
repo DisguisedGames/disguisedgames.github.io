9117ca49591a848aa95a696845aa8e85491cc7f8
JohanDev <johanclipss@gmail.com>
2021-12-12 15:30:41 +0100

    ?123123

--- components/Navbar.js
+++ components/Navbar.js
@@ -14,14 +14,12 @@ const Nav = styled.nav`
 const StyledLink = styled.a`
     padding: 0rem 2rem;
     justify-content: center;
-    filter: invert(93%) sepia(100%) saturate(28%) hue-rotate(45deg) brightness(107%) contrast(106%);
     Image { 
         font-size: 1.5rem;
         justify-content: center;
         align-items: center;
         width: 2rem;
         height: 2rem;
-        filter: invert(93%) sepia(100%) saturate(28%) hue-rotate(45deg) brightness(107%) contrast(106%);    
     }
 `;
 
@@ -33,17 +31,17 @@ const Navbar = () => {
             <div>
                 <Link href='/games' passHref>
                     <StyledLink>
-                        <Image src='https://res.cloudinary.com/disguised/image/upload/v1639299547/games_ujrjwn.svg' alt="Games" width='25rem' height='25rem' />
+                        <Image src='https://res.cloudinary.com/disguised/image/upload/v1639319393/navbar/games_w4p74d.png' alt="Games" width='25rem' height='25rem' />
                     </StyledLink>
                 </Link>
                 <Link href='/' passHref>
                     <StyledLink>
-                        <Image src='https://res.cloudinary.com/disguised/image/upload/v1639299547/home_joltfc.svg' alt="Home" width='25rem' height='25rem' />
+                        <Image src='https://res.cloudinary.com/disguised/image/upload/v1639319393/navbar/home_xxgjce.png' alt="Home" width='25rem' height='25rem' />
                     </StyledLink>
                 </Link>
                 <Link href='/settings' passHref>
                     <StyledLink>
-                        <Image src='https://res.cloudinary.com/disguised/image/upload/v1639299547/settings_gcgfx3.svg' alt="Settings" width='25rem' height='25rem' />
+                        <Image src='https://res.cloudinary.com/disguised/image/upload/v1639319393/navbar/settings_zl8i80.png' alt="Settings" width='25rem' height='25rem' />
                     </StyledLink>
                 </Link>
             </div>
