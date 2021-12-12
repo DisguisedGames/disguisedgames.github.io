e7aea75701184d7f2d3f10398f70476990716a03
JohanDev <johanclipss@gmail.com>
2021-12-12 14:16:25 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
 module.exports = {
     images: {
+        loader: 'cloudinary',
+        path: 'https://res.cloudinary.com/demo/image/upload/',
         domains: [
             'res.cloudinary.com',
         ]
