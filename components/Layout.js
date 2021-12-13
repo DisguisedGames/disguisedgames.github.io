06b7ba1625bd795b572d608bf79b5ed85dfc1e35
JohanDev <johanclipss@gmail.com>
2021-12-13 11:44:55 +0100

    Update Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -8,7 +8,6 @@ export default function Layout({children}) {
         <Navbar />
         {children}
         <Footer />
-        <Script src="html5.api.gamedistribution.com/libs/gd/api.js"></Script>
     </div>
   )
 }
